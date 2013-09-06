254f7991ca5f8181b798f789b2da546dd2fb2fdf
Ylan Segal <ylan.segal@mellmo.com>
2013-09-06 14:48:48 -0700

    Implement Token#code

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,3 +1,7 @@
 class Token < ActiveRecord::Base
   belongs_to :user
+
+  def code
+    ROTP::TOTP.new(secret)
+  end
 end
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Token do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:token) { Token.new :secret => 'some_secret' }
+
+  it 'generates a code' do
+    token.code.should_not be_nil
+  end
 end
