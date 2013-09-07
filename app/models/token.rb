8d5acf55f8196ce977a2827e354ad39330d4ef34
Ylan Segal <ylan.segal@mellmo.com>
2013-09-06 17:30:18 -0700

    Add secret validation

--- app/models/token.rb
+++ app/models/token.rb
@@ -2,8 +2,19 @@ class Token < ActiveRecord::Base
   belongs_to :user
 
   validates :name, :secret, :presence => true
+  validate :secret_is_valid
 
   def code
     ROTP::TOTP.new(self.secret).now
+  end
+
+  private
+
+  def secret_is_valid
+    begin
+      code
+    rescue
+      errors.add :secret, 'is not a valid base32 secret'
+    end
   end
 end
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -6,4 +6,9 @@ describe Token do
   it 'generates a code' do
     token.code.should_not be_nil
   end
+
+  it 'validates a secret is valid' do
+    token.secret = 'shh.....'
+    token.should_not be_valid
+  end
 end
