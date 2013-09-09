d2131bac799d6731682c57a6dfa18552a9c48931
Ylan Segal <ylan.segal@mellmo.com>
2013-09-08 19:43:01 -0700

    Go to tokens is logged in

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,10 +4,17 @@ describe HomeController do
   render_views
 
   describe "GET 'index'" do
+    let(:user) { FactoryGirl.create(:user) }
+
     it "should be successful" do
-      get 'index'
+      get :index
       response.should be_success
     end
-  end
 
+    it 'redirects to tokens if signed-in' do
+      sign_in user
+      get :index
+      response.should redirect_to(tokens_path)
+    end
+  end
 end
