cdffaed664ec02eb0607ce2fe700673483ff11a9
Ylan Segal <ylan.segal@mellmo.com>
2013-09-08 19:55:18 -0700

    Move secret to env variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Tokenator::Application.config.secret_key_base = 'b938e0f5bf563acaf73b919957ae445ddc2578928dfa3cadf4d37c1d9fd8485a4b0c1c728cb179e5d6b04a23744ff87a7a365b20662c1e67d2c4b2d1f6a04e33'
+Tokenator::Application.config.secret_key_base = ENV['RAILS_SECRET_TOKEN']
