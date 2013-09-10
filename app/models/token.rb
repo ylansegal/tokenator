d6714df0bae9049730501cccb49845bc040ba4ff
Ylan Segal <ylan.segal@mellmo.com>
2013-09-10 09:29:01 -0700

    Strip spaces in tokens, a la Google

--- app/models/token.rb
+++ app/models/token.rb
@@ -4,6 +4,8 @@ class Token < ActiveRecord::Base
   validates :name, :secret, :presence => true
   validate :secret_is_valid
 
+  before_validation :normalize_secret
+
   def code
     ROTP::TOTP.new(self.secret).now
   end
@@ -16,5 +18,9 @@ class Token < ActiveRecord::Base
     rescue
       errors.add :secret, 'is not a valid base32 secret'
     end
+  end
+
+  def normalize_secret
+    secret && secret.gsub!(' ', '')
   end
 end
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Token do
-  let(:token) { FactoryGirl.build(:token) }
+  subject(:token) { FactoryGirl.build(:token) }
 
   it 'generates a code' do
     token.code.should_not be_nil
@@ -10,5 +10,12 @@ describe Token do
   it 'validates a secret is valid' do
     token.secret = 'shh.....'
     token.should_not be_valid
+  end
+
+  it 'cleans whitespaces in secret before validating' do
+    secret = "qt4v gmlu 5f5i cvep"
+    token = Token.new :secret => secret
+    token.valid?
+    token.secret.should == "qt4vgmlu5f5icvep"
   end
 end
