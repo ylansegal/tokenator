c458b0b21c23b72acb38b362d8197d3363e90109
Ylan Segal <ylan@segal-family.com>
2013-11-12 20:28:12 -0800

    Upgrade to rails 4.0.1 ...
    - Fix view spec, broke in new version of rails because of routing

--- spec/views/tokens/index.html.haml_spec.rb
+++ spec/views/tokens/index.html.haml_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'tokens/index.html.haml' do
   it 'displays tokens starting with zero' do
-    token = Token.new
+    token = FactoryGirl.create(:token)
     token.stub(:code).and_return(12345)
     assign(:tokens, [token])
 
