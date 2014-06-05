ff913fcd90b117c0285fea5a049b1fdb1f5e4aa9
Ylan Segal <ylan@segal-family.com>
2014-06-05 09:45:20 -0700

    Enable partial doubles verification

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,4 +50,8 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = [:should, :expect]
   end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
 end
