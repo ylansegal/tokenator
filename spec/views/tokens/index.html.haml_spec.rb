860a30a3b82069fa850f1715248fef8204f7fe6a
Ylan Segal <ylan@segal-family.com>
2014-08-18 14:32:10 -0700

    Make allawances for rotp already returning padded string

--- spec/views/tokens/index.html.haml_spec.rb
+++ spec/views/tokens/index.html.haml_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe 'tokens/index.html.haml', type: :view do
   it 'displays tokens starting with zero' do
     token = FactoryGirl.create(:token)
-    expect(token).to receive(:code).and_return(12345)
+    expect(token).to receive(:code).and_return('070349')
     assign(:tokens, [token])
 
     render
 
-    expect(rendered).to include('012345')
+    expect(rendered).to include('070349')
   end
 end
