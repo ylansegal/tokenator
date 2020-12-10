c733f09ded01bab624af371f3a49fa4e55447ae7
Ylan Segal <ylan@segal-family.com>
2020-12-10 11:37:31 -0800

    Minor churn

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 2019_08_19_224708) do
     t.string "name"
     t.string "secret"
     t.integer "user_id"
-    t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "created_at"
     t.index ["user_id"], name: "index_tokens_on_user_id"
   end
 
